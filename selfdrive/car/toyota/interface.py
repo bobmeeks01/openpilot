a99d1ccae58e69a191b75e8dab7323c28c34b2d5
bobmeeks01 <58351215+bobmeeks01@users.noreply.github.com>
2020-01-28 18:48:16 -0700

    Steer Tuning

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -197,11 +197,11 @@ class CarInterface(CarInterfaceBase):
       stop_and_go = True
       ret.safetyParam = 73
       ret.wheelbase = 2.79
-      ret.steerRatio = 14.8  # 14.8 is spec end-to-end
+      ret.steerRatio = 14.0  # 14.8 is spec end-to-end
       tire_stiffness_factor = 0.5533  # not optimized yet
       ret.mass = 4387. * CV.LB_TO_KG + STD_CARGO_KG  # mean between min and max
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
-      ret.lateralTuning.pid.kf = 0.00007818594
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.4], [0.05]]
+      ret.lateralTuning.pid.kf = 0.00006    # 0.00007818594
 
     elif candidate == CAR.SIENNA:
       stop_and_go = True
